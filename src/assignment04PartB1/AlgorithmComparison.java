ad1ed1d3ced4ff174b0afb20dab9d9861ebd0850
mosguinz <mos.guinz@gmail.com>
2021-04-23 01:31:36 -0700

    Add code to time algorithms

--- src/assignment04PartB1/AlgorithmComparison.java
+++ src/assignment04PartB1/AlgorithmComparison.java
@@ -1,6 +1,90 @@
 package assignment04PartB1;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Random;
+
 public class AlgorithmComparison {
+
+    public static void main(String[] args) {
+        // Set this to true for CSV-like output.
+        boolean printToCsv = true;
+
+        // Path to output CSV file.
+        String fileName = "./src/assignment04PartB1/algo.csv";
+
+        // Disable print to console; reduce lag.
+        boolean printToConsole = true;
+
+        // `n` number of tests. 1 to `n`, inclusive.
+        int runUntil = 10000;
+
+        // Max array size.
+        int maxArrSize = 10000;
+
+        /* Spaghetti below ************************************************************************/
+        String humanFormat = "[n = %-8d] %-15s ns vs %-15s ns%n";
+        String csvFormat = "%d,%d,%d%n";
+        PrintWriter fOut = null;
+
+        try {
+            fOut = new PrintWriter(fileName);
+            fOut.println("n,Loop A (ns),Loop B (ns)");
+        } catch (FileNotFoundException e) {
+            System.err.println(e);
+        }
+
+        for (int n = 1; n <= runUntil; n++) {
+            long start, stop, deltaA, deltaB;
+            int[] arr = generateRandomInts(maxArrSize);
+
+            // Time loop A
+            start = System.nanoTime();
+            algoA(arr);
+            stop = System.nanoTime();
+            deltaA = stop - start;
+
+            // Time loop B
+            start = System.nanoTime();
+            algoB(arr);
+            stop = System.nanoTime();
+            deltaB = stop - start;
+
+            // Output in csv format
+            if (printToCsv) {
+                fOut.printf(csvFormat, n, deltaA, deltaB);
+            }
+
+            if (!printToConsole) {
+                continue;
+            }
+
+            // Output for human eyes
+            if (deltaA < deltaB) {
+                System.out.printf(humanFormat, n, colorFaster(deltaA), colorSlower(deltaB));
+            } else {
+                System.out.printf(humanFormat, n, colorSlower(deltaA), colorFaster(deltaB));
+            }
+        }
+        fOut.close();
+    }
+
+    private static String colorFaster(long d) {
+        return "\033[0;32m" + d + "\033[0m";
+    }
+
+    private static String colorSlower(long d) {
+        return "\033[0;31m" + d + "\033[0m";
+    }
+
+    private static int[] generateRandomInts(int size) {
+        int[] randInts = new int[size];
+        Random rand = new Random();
+        for (int i = 0; i < size; i++) {
+            randInts[i] = rand.nextInt((int) 2e32);
+        }
+        return randInts;
+    }
 
     /**
      * "Algorithm A searches the entire array sequentially and records the largest entry seen so
