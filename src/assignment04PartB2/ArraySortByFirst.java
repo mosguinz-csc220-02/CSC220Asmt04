05e91e9954ba7aeff684f4f43bf4e0c050a91a14
mosguinz <mos.guinz@gmail.com>
2021-04-23 02:06:53 -0700

    Add method to display array for 4E

--- src/assignment04PartB2/ArraySortByFirst.java
+++ src/assignment04PartB2/ArraySortByFirst.java
@@ -1,30 +1,34 @@
 package assignment04PartB2;
+
 /**
- *
  * Part B.2
- *
- * Please do not change any code in the main method 
- *
+ * <p>
+ * Please do not change any code in the main method
  */
-public class ArraySortByFirst
-{
-	public static void sortByFirst(int data[][])
-	{
-	}
+public class ArraySortByFirst {
 
-	public static void display(int data[][])
-	{
-	} 
+    public static void sortByFirst(int data[][]) {
+    }
 
-	//
+    public static void display(int data[][]) {
+        for (int[] row : data) {
+            System.out.print("     ");
+            for (int col : row) {
+                System.out.print(col + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    // @formatter:off
 	// Please do not change any code in the main method
 	//
 	public static void main(String args[])
 	{
-		int array[][] = {{1, 2, 3, 4, 5}, 
-				 {3, 4, 5, 1, 2}, 
-				 {5, 2, 3, 4, 1}, 
-				 {2, 3, 1, 4, 5}, 
+		int array[][] = {{1, 2, 3, 4, 5},
+				 {3, 4, 5, 1, 2},
+				 {5, 2, 3, 4, 1},
+				 {2, 3, 1, 4, 5},
 				 {4, 2, 3, 1, 5}};
 
 		System.out.println("The array is initially " );
