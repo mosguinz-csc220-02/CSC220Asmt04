c543ddaf1300aa24a5184297555ddaeddd718679
mosguinz <mos.guinz@gmail.com>
2021-04-23 22:01:45 -0700

    Add method to display priority table

--- src/assignment04PartE/SFSUOneStop.java
+++ src/assignment04PartE/SFSUOneStop.java
@@ -1,13 +1,23 @@
 package assignment04PartE;
 
 /**
- *
  * Part E
- *
  */
 
 import java.util.PriorityQueue;
 
 public class SFSUOneStop {
-	
+
+    private static final String tableFormat = "%10s%-6s   %-6s   %-6s   %-6s   %-6s   %-6s%n";
+
+    public static void display(PriorityQueue<Student> pq, String priority) {
+        System.out.printf("Priority: %s%n", priority);
+
+        while (pq.peek() != null) {
+            Student s = pq.poll();
+            System.out.printf(tableFormat, "", s.getFirstName(), s.getLastName(), s.getStudentId(),
+                    s.getGpa(), s.getSmallQuestionTotal(), s.getBigQuestionTotal());
+        }
+        System.out.println();
+    }
 }
