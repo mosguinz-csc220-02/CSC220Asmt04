ca4ed4dbbe729e472dce6490e38bdac23d26121c
mosguinz <mos.guinz@gmail.com>
2021-04-23 20:18:25 -0700

    Add fields and constructor for Student

--- src/assignment04PartE/Student.java
+++ src/assignment04PartE/Student.java
@@ -1,11 +1,26 @@
 package assignment04PartE;
 
 /**
- *
  * Part E
- *
  */
 
 public final class Student implements Comparable<Student> {
 
+    String firstName;
+    String lastName;
+    int studentId;
+    double gpa;
+    int smallQuestionTotal;
+    int bigQuestionTotal;
+
+    public Student(String firstName, String lastName, int studentId, double gpa,
+            int smallQuestionTotal,
+            int bigQuestionTotal) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.studentId = studentId;
+        this.gpa = gpa;
+        this.smallQuestionTotal = smallQuestionTotal;
+        this.bigQuestionTotal = bigQuestionTotal;
+    }
 }
